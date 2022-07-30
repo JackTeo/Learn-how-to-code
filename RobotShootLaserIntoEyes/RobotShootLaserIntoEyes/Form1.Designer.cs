65fa58d893f987007c14a74acb0e0305b408d168
Jack Teo <61779701+JackTeo@users.noreply.github.com>
2022-07-30 13:21:19 +1200

    Learn how to code

---
+++ RobotShootLaserIntoEyes/RobotShootLaserIntoEyes/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RobotShootLaserIntoEyes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 19200;
+            this.serialPort1.PortName = "COM4";
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(12, 9);
+            this.picBox.Margin = new System.Windows.Forms.Padding(0);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(649, 487);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(722, 243);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 10);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(332, 489);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(10, 206);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 498);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RobotShootLaserIntoEyes/RobotShootLaserIntoEyes/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing.Drawing2D;
+
+namespace RobotShootLaserIntoEyes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            ComboBox cboDevice = new ComboBox();
+            foreach (FilterInfo device in filter)
+            {
+                cboDevice.Items.Add(device.Name);
+            }
+            cboDevice.SelectedIndex = 0;
+
+            device = new VideoCaptureDevice(filter[cboDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+
+        }
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Get Screen size
+            int fullScreenY = picBox.Height;
+            int fullScreenX = picBox.Width;
+            int halfScreenY = fullScreenY / 2;
+            int halfScreenX = fullScreenX / 2;
+
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach(Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using(Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+
+                        int xpos = rectangle.X + (rectangle.Width / 2);
+                        int ypos = rectangle.Y + (rectangle.Height / 2);
+
+                        // Getting the center of the detected area
+                        int x = halfScreenX - xpos;
+                        int y = halfScreenY - ypos;
+
+                        // Debug use
+                        // Drawing small rectangle to make sure I picked up the center of detection
+                        Rectangle smallRec = new Rectangle();
+                        smallRec.X = xpos;
+                        smallRec.Y = ypos;
+                        smallRec.Width = 5;
+                        smallRec.Height = 5;
+                        // Draw rectangle
+                        graphics.DrawRectangle(pen, smallRec);
+
+                        // Getting angle using formula
+                        int degreeX = (int)(90 + tanCal(x));
+                        int degreeY = (int)(90 + tanCal(y));
+
+                        // Sending angle as output to Arduino
+                        serialPort1.Write(degreeY + "y");
+                        serialPort1.Write(degreeX + "x");
+
+
+                        //Console.WriteLine(degreeX);
+                        //Console.WriteLine(degreeY);
+
+                        //Console.WriteLine(tanCal(x));
+                        //Console.WriteLine(tanCal(y));
+
+                        //Console.WriteLine();
+                    }
+                }
+            }
+            picBox.Image = bitmap;
+        }
+
+        // Convert Distance to Angle formula
+        private double tanCal(double x)
+        {
+            // 690 is a static approximate distance from to my face in mm
+            // Change the value if your room is bigger or smaller
+            return Math.Atan(x/720) * 180 / Math.PI;
+        }
+    }
+}
